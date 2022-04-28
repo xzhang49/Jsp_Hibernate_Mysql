7ccb2e9d4cf8beaa3ce7e6da58be55033a8b6209
xzhang49 <xzhanguh@gmail.com>
2022-04-28 15:51:29 -0500

    1st commit

---
+++ login-app-hbm/src/main/java/com/demo/dao/UserDao.java
@@ -0,0 +1,53 @@
+package com.demo.dao;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import net.javaguides.hibernate.model.User;
+import net.javaguides.hibernate.util.HibernateUtil;
+
+public class UserDao {
+
+	public void saveUser(User user) {
+		Transaction transaction = null;
+		try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+			// start a transaction
+			transaction = session.beginTransaction();
+			// save the student object
+			session.save(user);
+			// commit transaction
+			transaction.commit();
+		} catch (Exception e) {
+			if (transaction != null) {
+				transaction.rollback();
+			}
+			e.printStackTrace();
+		}
+	}
+
+	public boolean validate(String userName, String password) {
+
+		Transaction transaction = null;
+		UserDao user = null;
+		try (Session session = com.demo.util.HibernateUtil.getSessionFactory().openSession()) {
+			// start a transaction
+			transaction = session.beginTransaction();
+			// get an user object
+			user = (User) session.createQuery("FROM User U WHERE U.userName = :userName").setParameter("userName", userName)
+					.uniqueResult();
+			
+			if(user != null && user.getPassword().equals(password)) {
+				return true;
+			}
+			// commit transaction
+			transaction.commit();
+		} catch (Exception e) {
+			if (transaction != null) {
+				transaction.rollback();
+			}
+			e.printStackTrace();
+		}
+		return false;
+	}
+
+}
